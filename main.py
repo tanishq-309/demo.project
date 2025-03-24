77af529c9c5d5e84e836a1672a7d31609b5c55fa
s24mcag0012 <test@gmail.com>
2025-03-24 09:49:49 +0530

    me

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from addition import Adittion
 from subtraction import Subtraction
+from multiplication import Multiplication
+from division import Division
 class Calculator:
     def __init__(self):
         self.run()
@@ -15,6 +17,10 @@ class Calculator:
                 print("Result",Adittion.add(num1,num2))
             elif choice == '2':
                 print("Result:", Subtraction.subtract(num1, num2))
+            elif choice == '3':
+                print("Result:", Multiplication.multiply(num1, num2))
+            elif choice == '4':
+                print("Result:", Division.divide(num1, num2))
         else:
             print("Invalid Input")
     
